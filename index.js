81aa7ff6ca53eb3a816c3b53f310b1fb4b2534d4
Dylan Swanson <81135334+Daswanson22@users.noreply.github.com>
2023-10-16 22:04:19 +0100

    Added json, project cards, line breaks for each section

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 $(document).ready(function() {
-    $(window).on('scroll', function() {
-        $("fixed-scroll").fadeOut('slow');
-    })
 
     $(".nav-link").hover(function() {
         var navItemChanges = {
@@ -16,4 +13,27 @@ $(document).ready(function() {
         };
         $(this).css(navItemChanges);
     })
+
+    fillCardInfo();
+
 })
+
+function fillCardInfo() {
+    fetch("projects.json").then((res) => {
+        return res.json();
+    }).then((data) => {
+        let cards = data.cards;
+        cards.forEach(card => {
+            let cardHTML = `<div class="card mb-3" id="${card.ID}">
+            <img class="card-img-top" src="${card.image}" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">${card.title}</h5>
+              <p class="card-text">${card.information}</p>
+              <a href="${card.link}" target="_blank" class="btn-green">View Here</a>
+            </div>
+          </div>`;
+            $("#cardContainer").append(cardHTML);
+        });
+
+    }).catch(error => console.error("Error fetching JSON", error));
+}
